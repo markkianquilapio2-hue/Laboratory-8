24c68dfdae7e26787e73b2908f7298aa8a8a8735
grezabethgalvesaraojo-code <grezabethgalvesaraojo@gmail.com>
2025-11-28 19:19:16 +0800

    first commit

---
+++ src/main/java/com/Quilapio_Lab8/Laboratory8/Laboratory8Application.java
@@ -0,0 +1,13 @@
+package com.Quilapio_Lab8.Laboratory8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratory8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratory8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Quilapio_Lab8/Laboratory8/Laboratory8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Quilapio_Lab8.Laboratory8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratory8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
